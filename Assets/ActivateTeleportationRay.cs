50f18598a78bd06c718a1e502a064d0c86c51fe2
Elijah Schoneweis <eschoneweis2@huskers.unl.edu>
2023-02-13 18:29:40 -0600

    Basic Game Menu

--- Assets/ActivateTeleportationRay.cs
+++ Assets/ActivateTeleportationRay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 
 public class ActivateTeleportationRay : MonoBehaviour
@@ -14,10 +15,18 @@ public class ActivateTeleportationRay : MonoBehaviour
     public InputActionProperty leftCancel;
     public InputActionProperty rightCancel;
 
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
+
     // Update is called once per frame
     void Update()
     {
-        leftTeleportation.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
